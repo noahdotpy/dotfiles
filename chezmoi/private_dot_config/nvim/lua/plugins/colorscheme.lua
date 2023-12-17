79302e7f9eb2c5e6442c13b163f114cffc3db2cc
noahdotpy <noahdotpy@proton.me>
2023-12-17 16:28:09 +1000

    add adwaita themes to nvim and kitty

--- chezmoi/private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ chezmoi/private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,15 @@ return {
 		priority = 1000,
 	},
 	{
+		"Mofiqul/adwaita.nvim",
+		lazy = true,
+		priority = 1000,
+		config = function()
+			vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+			vim.g.adwaita_transparent = true -- makes the background transparent
+		end,
+	},
+	{
 		"neanias/everforest-nvim",
 		lazy = true,
 		priority = 1000,
