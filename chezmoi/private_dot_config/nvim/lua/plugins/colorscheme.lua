4cd25e469c1069b525d787aa971c2b227da64a42
noahdotpy <noahdotpy@proton.me>
2024-01-06 11:24:45 +1000

    add oxocarbon nvim theme

--- chezmoi/private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ chezmoi/private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,31 @@
 return {
-	{
-		"ellisonleao/gruvbox.nvim",
-		lazy = true,
-		priority = 1000,
-	},
-	{
-		"Mofiqul/adwaita.nvim",
-		lazy = true,
-		priority = 1000,
-		config = function()
-			vim.g.adwaita_disable_cursorline = true -- to disable cursorline
-			vim.g.adwaita_transparent = true -- makes the background transparent
-		end,
-	},
-	{
-		"neanias/everforest-nvim",
-		lazy = true,
-		priority = 1000,
-		config = function()
-			require("everforest").setup({
-				background = "hard",
-			})
-		end,
-	},
+  {
+    "nyoom-engineering/oxocarbon.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+    priority = 1000,
+  },
+  {
+    "Mofiqul/adwaita.nvim",
+    lazy = true,
+    priority = 1000,
+    config = function()
+      vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+      vim.g.adwaita_transparent = true     -- makes the background transparent
+    end,
+  },
+  {
+    "neanias/everforest-nvim",
+    lazy = true,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        background = "hard",
+      })
+    end,
+  },
 }
