94d5329b17ec26312e6d2b7a14ab94f59248856a
noahdotpy <noahdotpy@proton.me>
2024-01-06 12:57:00 +1000

    nvim: add github theme

--- chezmoi/private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ chezmoi/private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,10 @@
 return {
   {
+    "projekt0n/github-nvim-theme",
+    lazy = true,
+    priority = 1000,
+  },
+  {
     "nyoom-engineering/oxocarbon.nvim",
     lazy = true,
     priority = 1000,
