4628489582b27f26db0abcb4fad3e95af2f6827f
noahdotpy <noahdotpy@proton.me>
2024-01-10 12:37:29 +1000

    chore(devshell): cleanup useless commands, add update command

--- devshell.nix
+++ devshell.nix
@@ -1,8 +1,7 @@
-{
-  inputs',
-  inputs,
-  pkgs,
-  ...
+{ inputs'
+, inputs
+, pkgs
+, ...
 }: {
   devShells = {
     default = inputs'.devshell.legacyPackages.mkShell {
@@ -13,54 +12,33 @@
         home-manager
         nix
       ];
-      commands = let
-        path = "$HOME/.local/share/chezmoi";
-        find = "${pkgs.findutils}/bin/find";
-        entr = "${pkgs.entr}/bin/entr";
+      commands =
+        let
+          path = "$HOME/.local/share/chezmoi";
 
-        findAllFiles = "${find} ${path}/{chezmoi,homemanager,flake.nix}";
-        findNixFiles = "${find} ${path}/{homemanager,flake.nix}";
-        findChezmoiFiles = "${find} ${path}/chezmoi";
-
-        applyAll = "$HOME/.local/bin/apply.sh";
-        applyHomeManager = "home-manager switch --flake ${path}";
-        applyChezmoi = "${pkgs.chezmoi}/bin/chezmoi apply";
-
-        watchAll = "${findAllFiles} | ${entr} ${applyAll}";
-        watchHomeManager = "${findNixFiles} | ${entr} ${applyHomeManager}";
-        watchChezmoi = "${findChezmoiFiles} | ${entr} ${applyChezmoi}";
-      in [
-        {
-          name = "watch";
-          command = "${watchAll}";
-          help = "watch chezmoi+homemanager";
-        }
-        {
-          name = "watch-homemanager";
-          command = "${watchHomeManager}";
-          help = "watch homemanger";
-        }
-        {
-          name = "watch-chezmoi";
-          command = "${watchChezmoi}";
-          help = "watch chezmoi";
-        }
-        {
-          name = "apply";
-          command = "${applyAll}";
-          help = "apply chezmoi+homemanager";
-        }
-        {
-          name = "apply-homemanager";
-          command = "${applyHomeManager}";
-          help = "apply homemanager";
-        }
-        {
-          name = "apply-chezmoi";
-          command = "${applyChezmoi}";
-          help = "apply chezmoi";
-        }
-      ];
+          apply = "$HOME/.local/bin/apply.sh";
+          watch = "
+            ${pkgs.findutils}/bin/find ${path}/{chezmoi,homemanager,flake.nix} |
+            ${pkgs.entr}/bin/entr ${apply}
+           ";
+        in
+        [
+          {
+            name = "watch";
+            command = "${watch}";
+            help = "automatically apply when changes are detected";
+          }
+          {
+            name = "apply";
+            command = "${apply}";
+            help = "apply changes from chezmoi and homemanager/";
+          }
+          {
+            name = "update";
+            command = "nix flake update ${path}";
+            help = "update nix inputs";
+          }
+        ];
       name = "dots";
     };
   };
