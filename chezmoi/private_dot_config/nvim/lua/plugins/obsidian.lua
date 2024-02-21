1e906851627e58a27e382c0d7bd567adb3e0ff7f
noahdotpy <noahdotpy@proton.me>
2024-02-21 18:24:18 +1000

    feat(nvim): add obsidian plugin

---
+++ chezmoi/private_dot_config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+      "BufReadPre"
+      .. vim.fn.expand("~")
+      .. "Documents/vaults/**.md",
+      "BufNewFile" .. vim.fn.expand("~") .. "Documents/vaults/**.md",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/Documents/vaults/personal",
+        },
+      },
+    },
+  },
+}
