3b5815d0bab7e3ca4d89c3eb3ed73e6fbcf96099
noahdotpy <noahdotpy@proton.me>
2024-05-26 08:12:09 +1000

    chore: update nvim config: use catppuccin, add oil plugin

--- chezmoi/private_dot_config/nvim/lua/config/init.lua
+++ chezmoi/private_dot_config/nvim/lua/config/init.lua
@@ -12,7 +12,7 @@ require("lazy").setup({
 		{
 			"LazyVim/LazyVim",
 			import = "lazyvim.plugins",
-			opts = { colorscheme = "gruvbox" },
+			opts = { colorscheme = "catppuccin" },
 		},
 		{ import = "plugins" },
 	},
--- chezmoi/private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ chezmoi/private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,36 +1,41 @@
 return {
-  {
-    "projekt0n/github-nvim-theme",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "nyoom-engineering/oxocarbon.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = true,
-    priority = 1000,
-  },
-  {
-    "Mofiqul/adwaita.nvim",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      vim.g.adwaita_disable_cursorline = true -- to disable cursorline
-      vim.g.adwaita_transparent = true     -- makes the background transparent
-    end,
-  },
-  {
-    "neanias/everforest-nvim",
-    lazy = true,
-    priority = 1000,
-    config = function()
-      require("everforest").setup({
-        background = "hard",
-      })
-    end,
-  },
+	{
+		"projekt0n/github-nvim-theme",
+		lazy = true,
+		priority = 1000,
+	},
+	{
+		"nyoom-engineering/oxocarbon.nvim",
+		lazy = true,
+		priority = 1000,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = true,
+		priority = 1000,
+	},
+	{
+		"Mofiqul/adwaita.nvim",
+		lazy = true,
+		priority = 1000,
+		config = function()
+			vim.g.adwaita_disable_cursorline = true -- to disable cursorline
+			vim.g.adwaita_transparent = true -- makes the background transparent
+		end,
+	},
+	{
+		"neanias/everforest-nvim",
+		lazy = true,
+		priority = 1000,
+		config = function()
+			require("everforest").setup({
+				background = "hard",
+			})
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		lazy = true,
+		priority = 1000,
+	},
 }
