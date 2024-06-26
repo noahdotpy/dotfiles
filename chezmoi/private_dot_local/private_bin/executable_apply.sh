1856e3fcd74dcc89855f1880ab367a44846c36a9
noahdotpy <noahdotpy@proton.me>
2024-06-26 12:13:04 +1000

    fix: remove redundant scripts

--- chezmoi/private_dot_local/private_bin/executable_apply.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "## APPLYING CHEZMOI ##"
-chezmoi apply
-echo "## DONE APPLYING CHEZMOI ##"
-echo "## Applying HOMEMANAGER CONFIGURATION ##"
-home-manager switch --flake $HOME/.local/share/chezmoi/
-echo "## DONE APPLYING HOMEMANAGER CONFIGURATION ##"
