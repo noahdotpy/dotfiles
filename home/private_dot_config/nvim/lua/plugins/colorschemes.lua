56d45e17b2cc0ad2b63f82cf6864612e6351d906
noahdotpy <noahdotpy@proton.me>
2024-11-19 18:57:56 +1000

    chore: update neovim, systemd services, waybar and kitty

--- home/private_dot_config/nvim/lua/plugins/colorschemes.lua
+++ home/private_dot_config/nvim/lua/plugins/colorschemes.lua
@@ -1,19 +1,24 @@
 return {
   {
+    "neanias/everforest-nvim",
+    version = false,
+    priority = 1000,
+  },
+  {
     "catppuccin/nvim",
     lazy = true,
     priority = 1000,
   },
-  {
-    "f-person/auto-dark-mode.nvim",
-    opts = {
-      update_interval = 1000,
-      set_dark_mode = function()
-        vim.cmd("colorscheme catppuccin-mocha")
-      end,
-      set_light_mode = function()
-        vim.cmd("colorscheme catppuccin-latte")
-      end,
-    },
-  },
+  -- {
+  --   "f-person/auto-dark-mode.nvim",
+  --   opts = {
+  --     update_interval = 1000,
+  --     set_dark_mode = function()
+  --       vim.cmd("colorscheme catppuccin-mocha")
+  --     end,
+  --     set_light_mode = function()
+  --       vim.cmd("colorscheme catppuccin-latte")
+  --     end,
+  --   },
+  -- },
 }
