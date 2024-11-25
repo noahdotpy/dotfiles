abf9f657efc535e019b806b0b898d5db0d6fde44
noahdotpy <noahdotpy@proton.me>
2024-11-25 12:19:20 +1000

    feat(nushell): add direnv

--- devshell.nix
+++ devshell.nix
@@ -1,8 +1,7 @@
-{
-  inputs',
-  inputs,
-  pkgs,
-  ...
+{ inputs'
+, inputs
+, pkgs
+, ...
 }: {
   devShells = {
     default = inputs'.devshell.legacyPackages.mkShell {
@@ -12,34 +11,35 @@
         pkgs.git
         pkgs.nix
         pkgs.home-manager
-        pkgs.system-manager
-        inputs'.system-manager
+        # inputs'.system-manager
       ];
-      commands = let
-        path = "$HOME/.local/share/chezmoi";
+      commands =
+        let
+          path = "$HOME/.local/share/chezmoi";
 
-        apply = "$HOME/.local/bin/apply.sh";
-        watch = "
+          apply = "$HOME/.local/bin/apply.sh";
+          watch = "
             ${pkgs.findutils}/bin/find ${path}/{chezmoi,homemanager,flake.nix} |
             ${pkgs.entr}/bin/entr ${apply}
            ";
-      in [
-        {
-          name = "watch";
-          command = "${watch}";
-          help = "automatically apply when changes are detected";
-        }
-        {
-          name = "apply";
-          command = "${apply}";
-          help = "apply changes from chezmoi and homemanager/";
-        }
-        {
-          name = "update";
-          command = "nix flake update ${path}";
-          help = "update nix inputs";
-        }
-      ];
+        in
+        [
+          {
+            name = "watch";
+            command = "${watch}";
+            help = "automatically apply when changes are detected";
+          }
+          {
+            name = "apply";
+            command = "${apply}";
+            help = "apply changes from chezmoi and homemanager/";
+          }
+          {
+            name = "update";
+            command = "nix flake update ${path}";
+            help = "update nix inputs";
+          }
+        ];
       name = "dots";
     };
   };
--- home/private_dot_config/nushell/autorun.nu
+++ home/private_dot_config/nushell/autorun.nu
@@ -1,3 +1,4 @@
 # Nushell Autorun File
 
 try { kjv (^sort -R $"($env.HOME)/.local/share/zsh-bible/verses" | head -n 1) | cowsay | head -n -5 | lolcat -S 25 }
+
