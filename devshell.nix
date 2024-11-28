9072416c3276252d0db1d6cc5eda3a8bf67e613b
noahdotpy <noahdotpy@proton.me>
2024-11-28 20:37:45 +1000

    chore: remove devshell commands

--- devshell.nix
+++ devshell.nix
@@ -9,38 +9,10 @@
         pkgs.alejandra
         pkgs.chezmoi
         pkgs.git
-        pkgs.nix
         pkgs.home-manager
-        # inputs'.system-manager
+        pkgs.nix
       ];
-      commands =
-        let
-          path = "$HOME/.local/share/chezmoi";
-
-          apply = "$HOME/.local/bin/apply.sh";
-          watch = "
-            ${pkgs.findutils}/bin/find ${path}/{chezmoi,homemanager,flake.nix} |
-            ${pkgs.entr}/bin/entr ${apply}
-           ";
-        in
-        [
-          {
-            name = "watch";
-            command = "${watch}";
-            help = "automatically apply when changes are detected";
-          }
-          {
-            name = "apply";
-            command = "${apply}";
-            help = "apply changes from chezmoi and homemanager/";
-          }
-          {
-            name = "update";
-            command = "nix flake update ${path}";
-            help = "update nix inputs";
-          }
-        ];
-      name = "dots";
+      name = "myhome";
     };
   };
 }
